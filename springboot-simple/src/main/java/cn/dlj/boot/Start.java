59d964a57373a2370b1d352a8d7c7f79afb01d23
qq443672581 <yxx@144830>
2018-03-06 21:13:20 +0800

    init

---
+++ springboot-simple/src/main/java/cn/dlj/boot/Start.java
@@ -0,0 +1,17 @@
+package cn.dlj.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication
+public class Start {
+
+	public static void main(String[] args){
+
+		new SpringApplication(Start.class).run(args);
+
+	}
+
+}
---
+++ springboot-simple/src/main/java/cn/dlj/boot/controller/CoreController.java
@@ -0,0 +1,18 @@
+package cn.dlj.boot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@RequestMapping("/")
+@Controller
+public class CoreController {
+
+    @RequestMapping("")
+    @ResponseBody
+    public String index() {
+        return "this is index!";
+    }
+
+
+}
