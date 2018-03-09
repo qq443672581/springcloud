01009c0104a185381c975384fc52223d5835ab4d
qq443672581 <yxx@144830>
2018-03-09 22:55:16 +0800

    init

--- springcloud-eureka-server-cluster-2/src/main/java/cn/dlj1/springcloud/EurekaServerCluster2Start.java
+++ springcloud-eureka-server-cluster-2/src/main/java/cn/dlj1/springcloud/EurekaServerCluster2Start.java
@@ -1,17 +1,15 @@
 package cn.dlj1.springcloud;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableEurekaClient
 @SpringBootApplication
-public class EurekaClientStart {
+public class EurekaServerCluster2Start {
 
     public static void main(String[] args) {
-        new SpringApplicationBuilder(EurekaClientStart.class).web(true).run(args);
+        new SpringApplicationBuilder(EurekaServerCluster2Start.class).web(true).run(args);
     }
 
 }
