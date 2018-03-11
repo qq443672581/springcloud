696fb92a8f0265606b9cb66c3e611fe5cb2a9ae1
qq443672581 <yxx@144830>
2018-03-11 21:40:38 +0800

    init

---
+++ springcloud-zuul/src/main/java/cn/dlj1/springcloud/ZuulStart.java
@@ -0,0 +1,17 @@
+package cn.dlj1.springcloud;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableZuulProxy
+@EnableEurekaClient
+@SpringBootApplication
+public class ZuulStart {
+
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(ZuulStart.class).web(true).run(args);
+    }
+
+}
