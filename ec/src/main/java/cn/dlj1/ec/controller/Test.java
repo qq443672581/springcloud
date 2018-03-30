cb47af844f18bf35dfa3245abd80b75e2e8e859a
fivewords <443672581@qq.com>
2018-03-30 18:36:16 +0800

    默认的

--- ec/src/main/java/cn/dlj1/ec/controller/Test.java
+++ ec/src/main/java/cn/dlj1/ec/controller/Test.java
@@ -3,6 +3,8 @@ package cn.dlj1.ec.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * @author: fivewords(443672581 @ qq.com)
@@ -11,8 +13,19 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class Test {
 
-    @Autowired
-    private JdbcTemplate template;
+    //@Autowired
+    //private JdbcTemplate template;
 
+    @RequestMapping("/")
+    @ResponseBody
+    public String index(String re){
+        System.out.println("请求" + re + ":1");
+        try {
+            Thread.sleep(2000);
+        }catch (Exception e){
+        }
+        System.out.println("请求" + re + ":2");
+        return "666";
+    }
 
 }
