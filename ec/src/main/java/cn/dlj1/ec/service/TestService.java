b98024d787c6efb0038ea2568154cf1e64b7f3da
qq443672581 <yxx@144830>
2018-04-01 22:03:32 +0800

    init

---
+++ ec/src/main/java/cn/dlj1/ec/service/TestService.java
@@ -0,0 +1,15 @@
+package cn.dlj1.ec.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * @author: fivewords(443672581 @ qq.com)
+ * @dateTime: 2018/3/31
+ */
+@Service
+public class TestService {
+
+    public String go() {
+        return "66";
+    }
+}
--- ec/src/test/java/cn/dlj1/springboot/SpringbootApplicationTests.java
+++ ec/src/test/java/cn/dlj1/springboot/SpringbootApplicationTests.java
@@ -1,9 +1,25 @@
 package cn.dlj1.springboot;
 
+import cn.dlj1.ec.ECStart;
+import cn.dlj1.ec.service.TestService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = ECStart.class)
 public class SpringbootApplicationTests {
 
+    @Autowired
+    private TestService testService;
 
+    @Test
     public void contextLoads() {
+
+        System.out.println(testService.go());
+
     }
 
 }
